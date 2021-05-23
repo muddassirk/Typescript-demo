f398fb421df90c4b931326994623323623ad8933
muddassirk <mkkhanu72@gmail.com>
2021-05-23 23:41:22 +0500

    add some code

--- src/sample1.js
+++ src/sample1.js
@@ -1,1 +1,6 @@
 console.log('hello TypeScript');
+// let a = 1;
+// a= true;
+// a = [1,2,3,4,5]
+var std = {};
+console.log(std.name);
--- src/sample1.ts
+++ src/sample1.ts
@@ -1,1 +1,34 @@
-console.log('hello TypeScript')
+// console.log('hello TypeScript')
+
+
+// let a = 1;
+// a= true;
+// a = [1,2,3,4,5]
+
+
+// let std = {}
+// console.log(std.name);
+
+
+// function a(b) {
+//     return b/2
+// }
+
+// a(2)
+// // a("z")
+
+
+
+// function a(b : number) {
+//     return b/2
+// }
+
+// // a(2)
+// a("z")
+
+
+let firstName: string = "Muhammad";
+let lastName: string = "khan";
+let age: number = 20;
+let isActiveStudent: boolean = true;
+let courses: string[] = ["TS", "JS", "React"];
